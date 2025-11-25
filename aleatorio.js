c29bded3505c2dcb39cdc5d4534d0b4a09077b6e
yasmin Banck <145784815+YasminBanck@users.noreply.github.com>
2025-11-25 12:15:03 -0300

    25/11

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,9 @@
+const nomes = ["Yasmin","Vitor","Amanda","Giovana","vinicius","Yago"];
+
+
 export function aleatorio(lista){
 const posicao = Math.floor(Math.random()*lista.length);
 return lista[posicao];
 }
+
+export const nome = aletorio(nomes);
